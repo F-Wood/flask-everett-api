bf9d70574afb25b62e54d68ca0c068dbc0937215
Fabien Dubois <fabiendubois@Fabiens-MacBook-Pro.local>
2025-05-16 10:57:57 +0200

    Corrige port et host pour Render

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import random
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -28,4 +29,5 @@ def explore():
     return jsonify(random.choice(EXPLORATIONS))
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
